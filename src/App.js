dea2e97a23a78f2b516501bbf16df4838c7ae968
fidelwx1992 <fidelherrera1992@gmail.com>
2019-09-26 18:09:02 -0400

    breakingbad

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios'
+
+function Frase({frase}) {
+  return (
+    <div className="frase">
+      <h1>{frase.quote}</h1>
+      <p>- {frase.author}</p>
+    </div>
+  )
+}
 
 function App() {
+
+  const [frase, obtenerFrase] = useState ({});
+
+  const consultarAPI = async () => {
+    const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    
+    //agregar resultado de la API al state (similar a this.setState)
+    obtenerFrase(resultado.data[0]);
+  }
+
+  //consulta a rest api
+  useEffect(() => {
+    consultarAPI()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+      <Frase 
+        frase={frase}
+      />
+      <button
+        onClick={consultarAPI}
+      >Generar Nueva</button>
     </div>
   );
 }
